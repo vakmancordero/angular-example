a9f2fd854a2d50900292f63e8bcd93dca449872f
Arturo Cordero <arturo.cordero@bsdenterprise.com>
2019-06-20 11:39:01 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LicenseAcquisitionComponent } from './license-acquisition/license-acquisition.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/license', pathMatch: 'full'},
+    {path: 'license', component: LicenseAcquisitionComponent},
+    {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LicenseAcquisitionComponent,
+        LoginComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/license-acquisition/license-acquisition.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-license-acquisition',
+  templateUrl: './license-acquisition.component.html',
+  styleUrls: ['./license-acquisition.component.css']
+})
+export class LicenseAcquisitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
